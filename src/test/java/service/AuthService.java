c80dba2803c2a46c1c4aa4c657138bf04ea3008d
MichelEduardoBZ <michel.zeschau@gmail.com>
2024-10-28 20:38:05 -0300

    Corrigido pipeline

--- src/test/java/service/AuthService.java
+++ src/test/java/service/AuthService.java
@@ -2,25 +2,12 @@ package service;
 
 import br.com.michel.lixo.dto.user.UserInsertDTO;
 import br.com.michel.lixo.service.ResourceNotFoundException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.networknt.schema.JsonSchema;
-import com.networknt.schema.JsonSchemaFactory;
-import com.networknt.schema.SpecVersion;
-import com.networknt.schema.ValidationMessage;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 
